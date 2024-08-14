e4d05cfaf54e6c49e648d8ab7e0a2b6f4e14243b
Aakash-Tripathi <aakash.tripathi0304@gmail.com>
2024-08-14 10:07:41 -0400

    radimagenet now supported

--- honeybee/models/__init__.py
+++ honeybee/models/__init__.py
@@ -2,3 +2,4 @@ from .REMEDIS.remedis import REMEDIS
 from .TissueDetector.tissue_detector import TissueDetector
 from .HuggingFaceEmbedder.embedder import HuggingFaceEmbedder
 from .UNI.uni import UNI
+from .RadImageNet.radimagenet import RadImageNet
