e5450eadba1d207f606f6725e4ad845b2d58bc4a
Aakash-Tripathi <aakash.tripathi0304@gmail.com>
2025-10-13 21:57:04 -0400

    simplifying for pypi

--- results/classification/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Utilities for cancer classification.
-"""
-
-from .data_loader import load_embeddings, load_saved_embeddings
-
-__all__ = ["load_embeddings", "load_saved_embeddings"]
--- results/retrieval/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Utilities for retrieval benchmarking.
-"""
-
-from .data_loader import load_embeddings, load_saved_embeddings
-
-__all__ = ["load_embeddings", "load_saved_embeddings"]
